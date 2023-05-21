c5f2d6f33ccb96e22154470f73f55deb68f88291
farhan <farhan204@gmail.com>
2023-05-21 12:51:15 +0500

    first

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include <string>
 #include <fstream>
 #include <iomanip>
+#include <sstream>
+
+
 using namespace std;
 
 //Structures 
@@ -15,12 +18,12 @@ typedef struct users{
 typedef struct Product {
     string name;
     double price;
-    int quantity, p_id=1;
+    int quantity;
 }pro;
 
+int p_id=1,u_id=1,l_id = 0,l_total=0,pushid = 0;
 string l_mail,l_name;
-bool found = false,admin= false;  
-int p_id=1;
+bool found = false,admin = false; 
 
 
 
@@ -36,9 +39,9 @@ inline void dashboard(); //Admin Dashboard program
 inline void list_user(); //list of users program
 inline void Add_pro(); //Add products program
 inline void list_pro(); //product program
-
-
-
+inline void l_tot(); //total bill program
+inline void checkout();//checkout
+void deleteRecord(int);//order complete
 
 
 
@@ -47,42 +50,240 @@ inline void list_pro(); //product program
 
 int main() {
 
-//	load(); // Loading Screen Function	
+
+	load(); // Loading Screen Function	
 
 	wel_menu(); // welcome menu function
+
 	if(found == true)
 		product(); //user menu
 	if(admin == true)
 		dashboard();	//admindashboard
-
 	return 0;
 }
 
+inline void checkout(){
+    system("cls");
+    head();
+    
+	cout<<"\t\t\t\t\t\t\b\b\b\b...........................\n\t\t\t\t\t       Checkout       \t\t\tYour Total Bill is Rs. "<<l_total<<"\n\t\t\t\t\t\t\b\b\b\b...........................\n\n";
+    
+	
+	ifstream check("order.txt");
 
+    cout << "\t\t+-------------------------------------------------------------------------------+\n"<< "\t\t|    Product ID    |\t  Name\t   |  Quantity   |\tPrice\t|  Total Price\t|\n"<< "\t\t+-------------------------------------------------------------------------------+\n";
 
-inline void product(){
+    int checkid, checkqty,checkprice,check_p_id;
+    string checkname;
+    while (check >> checkid >> checkname >> checkprice >> checkqty>>check_p_id) {
+       if(l_id == checkid){
+	    cout << "\t\t| " << setw(9) << check_p_id << setw(9)<<" | ";
+        cout  <<setw(9) << checkname << setw(8)<< " | ";
+        cout  << setw(8) << checkqty << setw(3)<< "\t |";
+		cout << setw(5) <<"Rs. "<< checkprice << setw(5)<< "\t| ";
+		cout << setw(5) <<"Rs. "<< checkprice*checkqty << setw(5)<< "\t| \n";
+		}
+    }
+    cout << "\t\t+-------------------------------------------------------------------------------+\n";	     
+	check.close();
+	
+	ifstream filling("push.txt");
+	int pushid,counter =0,count,price;
+	string name;
+	
+	while (filling >> pushid >> name >> count>>price) {
+	if(l_id == count)
+		counter ++;
+	}  
+	filling.close();
+	
+	if(counter >= 5){
+    double discount = l_total - (l_total * 0.30);
+		cout<<"\t\t\t\t\t\t\b\b\b\b...........................\n\t\t\t\t\t       After 30% Discount\n\t\t\t\t\t\tYour Total Bill is Rs. "<<discount<<"\n\t\t\t\t\t\t\b\b\b\b...........................\n\n";
+	}
+	else{
+			cout<<"\t\t\t\t\t\t\b\b\b\b...........................\n\t\t\t\t\t       Your Total Bill is Rs. "<<l_total<<"\t\t\t\t\t\t\n\t\t\t\t\t\t\b\b\b\b...........................\n\n";
+	}
+	
+	string out;
+	cout<<"\n\n\t\tEnter any key to proceed to checkout : ";
+	cin.ignore();
+	getline(cin,out);
+	deleteRecord(l_id);
 	system("cls");
 	head();
-
-	ifstream file("products.txt");
-    
-	string storedname;
-	int storeid,storedqty;
-	double storedprice;
-	 
-    while (file>>storeid >> storedname >> storedprice >> storedqty) {	
-    cout << "+------------------------------------+\n"
-    	<< "|                                    |\n"
-    	<< "|        " << storedname << "\n"		
-    	<< "|                                    |\n"
-    	<< "|    	 " << storedqty << "\n"
-    	<< "|                                    |\n"
-    	<< "|     	Rs." << storedprice << "\n"
-    	<< "|                                    |\n"
-    	<< "+------------------------------------+\n";  
-	}
-	file.close();
+	cout<<"\t\t\t\t\t\t\b\b\b\b...........................\n\t\t\t\t\t     Your Order is Placed \n\t\t\t\t\t\tThank You for shopping Here       \t\t\t\n\t\t\t\t\t\t\b\b\b\b...........................\n\n";
+	Sleep(2000);
+	main();
 }
+
+
+void deleteRecord(int idToDelete) {
+    ifstream inputFile("order.txt");
+    ofstream outputFile("temp.txt", ios::app);
+    ofstream outputFile1("push.txt", ios::app);
+
+    int u_id;
+    string p_name;
+    int price, qty, p_id;
+
+    while (inputFile >> u_id >> p_name >> price >> qty >> p_id) {
+        if (u_id != idToDelete) {
+            outputFile << u_id << "\t" << p_name << "\t" << price << "\t" << qty << "\t" << p_id << endl;
+        }
+        else{
+        	ifstream fill("push.txt");
+        	int orderid=0;
+        		while (fill >> orderid) {
+				pushid = orderid;
+			}  
+			fill.close();
+			pushid++;
+        	outputFile1 << pushid << "\t" << l_name << "\t" << l_id << "\t" << l_total << endl;
+        	outputFile1.close();
+		}
+    }
+
+    inputFile.close();
+    outputFile.close();
+
+    if (remove("order.txt") != 0) {
+        perror("Error deleting the file.");
+        return;
+    }
+
+    if (rename("temp.txt", "order.txt") != 0) {
+        perror("Error renaming the file.");
+        return;
+    }
+}
+
+
+
+
+inline void l_tot(){
+	ifstream total("order.txt");
+	int o_id,o_price,o_qty,o_p_id;
+	string o_p_name;
+	l_total = 0;
+	while(total>>o_id>>o_p_name>>o_price>>o_qty>>o_p_id){
+		if(l_id == o_id){
+			l_total += (o_price*o_qty);
+		}
+	}
+	total.close();
+}
+
+
+
+
+inline void product() {
+	l_tot();
+	string order1;
+	int order2,qty;
+	bool ord = false;
+    system("cls");
+    head();
+    
+	cout<<"\t\t\t\t\t\t\b\b\b\b...........................\n\t\t\t\t\t       Welcome "<< l_name<<"\t\t\tYour Total Bill is Rs. "<<l_total<<"\n\t\t\t\t\t\t\b\b\b\b...........................\n\n";
+    
+	
+	ifstream file("products.txt");
+
+    cout << "\t\t+---------------------------------------------------------------+\n";
+    cout << "\t\t|    Product ID    |\t  Name\t   |  Quantity   |\tPrice\t|\n";
+    cout << "\t\t+---------------------------------------------------------------+\n";
+
+    int storeid, storedqty;
+    double storedprice;
+    string storedname;
+    
+    while (file >> storeid >> storedname >> storedprice >> storedqty) {
+        cout << "\t\t| " << setw(9) << storeid << setw(9)<<" | ";
+        cout  <<setw(9) << storedname << setw(8)<< " | ";
+        cout  << setw(8) << storedqty << setw(3)<< "\t |";
+		cout << setw(5) <<"Rs. "<< storedprice << setw(5)<< "\t| \n";
+    }
+    
+    cout << "\t\t+---------------------------------------------------------------+\n";
+    
+	file.close();     
+    cout<<"\n\n\tEnter the product ID number to add to cart or press any number (Except Product ID) to go on checkout : ";
+    cin>>order1;
+
+    stringstream ss(order1);
+    ss >> order2;
+
+	ifstream fl("products.txt");
+  
+    while (fl >> storeid >> storedname >> storedprice >> storedqty) {
+    	
+    	if(order2 == storeid){
+		
+			quantity : 
+				cout<<"\n\t\tEnter Quantity of the product : ";
+    			cin>>qty;
+    	if(qty<=storedqty){
+    	
+    		ofstream order("order.txt",ios::app);
+			order<<l_id<<"\t"<<storedname<<"\t"<<storedprice<<"\t"<<qty<<"\t"<<storeid<<endl;
+			order.close();
+			
+    		ord = true;
+    		char cont;
+    		cout<<"\t\t\t\tAdded Successfully\n\t\tDo you want to continue your shopping (Y) for yes (C) for Checkout and (L) for logout : ";
+    		cin.ignore();
+			cin>>cont;
+    		
+			if(cont =='Y' || cont == 'y')
+    		product();
+			else if(cont =='C' || cont == 'c'){
+    		l_tot();
+			checkout();    		
+			}
+			else
+			main();
+		}
+		else 
+		goto quantity;
+	}
+		
+   }
+    
+	fl.close();	
+	
+	if(ord == false){
+		checkout();
+	}
+}
+
+
+
+/*
+inline void product() {
+    system("cls");
+    head();
+
+    ifstream file("products.txt");
+    
+    string storedname;
+    int storeid, storedqty;
+    double storedprice;
+    
+    while (file >> storeid >> storedname >> storedprice >> storedqty) {
+        cout << "+------------------------------------+\n"
+             << "|                                    |\n"
+             << "|        " << storedname << "\n"
+             << "|                                    |\n"
+             << "|         " << storedqty << "\n"
+             << "|                                    |\n"
+             << "|       Rs." << storedprice << "\n"
+             << "|                                    |\n"
+             << "+------------------------------------+\n";
+    }
+    file.close();
+}*/
+
 
 
 
@@ -147,7 +348,6 @@ inline void Add_pro(){
     } 
     
 	p_id++;
-    cout<<p_id;
     fi.close();
     
     cout << "\tEnter product name: ";
@@ -163,6 +363,7 @@ inline void Add_pro(){
     ofstream file("products.txt", ios::app);
     file << p_id<<"\t" << p.name << "\t" << p.price << "\t" << p.quantity << endl;
     file.close();
+    
     cout<<"\n\n\t\t\t\tProduct Added Successfully\n";
     cout<<"\n\t\t If you want to add more products press 1 : ";
 	string option;
@@ -253,48 +454,54 @@ inline void dashboard(){
 
 
 
-inline void login(){
+inline void login() {
     string mail, password;
-    login:
-    cout<<"\tType \"back\" go to back\n\n\t\t\tEnter your details to login \n\n";
-    cout << "\tEnter mail: ";
-	cin.ignore();
-	cin >> mail;
 
-    if(mail == "back"||mail == "BACK"||mail == "Back")
+login:
+    cout << "\tType \"back\" to go back\n\n\t\t\tEnter your details to login\n\n";
+    cout << "\tEnter mail: ";
+    cin >> mail;
+
+    if (mail == "back" || mail == "BACK" || mail == "Back")
         wel_menu();
 
     cout << "\tEnter password: ";
-	cin.ignore();
-	cin >> password;
+    cin >> password;
+    cin.ignore(); 
 
-    if(password == "back"||password == "BACK"||password == "Back")
+    if (password == "back" || password == "BACK" || password == "Back")
         wel_menu();
 
-    ifstream file("user.txt");
+    ifstream login("user.txt");
     string storedname, storedmail, storedPassword;
-
-    while (file >> storedname >> storedmail >> storedPassword) {
+    int loginid;
+    
+    while (login >>loginid >> storedname >> storedmail >> storedPassword) {
         if (storedmail == mail && storedPassword == password) {
-            l_mail = mail;
+            l_id = loginid;
+			l_mail = mail;
             l_name = storedname;
-            product();
             found = true;
             break;
         }
+        else if (mail == "admin" && password == "admin") {
+        	admin = true;
+        	break;
+		}
     }
-    file.close();
-    if(mail == "admin" && password =="admin"){
-  		admin = true;
-	}
-	if(admin != true || found != true) {
-        cout<<"\n\n\t\t\t\tWrong Credentials\n";
+    login.close();
+    
+    if (admin == false && found == false) {
+        cout << "\n\n\t\t\t\tWrong Credentials\n";
         Sleep(800);
         system("cls");
         head();
         goto login;
     }
 }
+
+
+
 
 
 
@@ -321,10 +528,11 @@ inline void registered(){
 		}
 		ifstream check("user.txt");
 		string u_name,u_password,u_mail;
+		int storeid;
 		
-		while(check >> u_name >> u_mail >> u_password){
+		while(check >>storeid>> u_name >> u_mail >> u_password){
 			if(u_mail == use.mail){
-				cout<<"Email Already Exists. please try different\n";
+				cout<<"\n\t\t\tEmail Already Exists. please try different\n";
 				goto mail;
 		}
 	}
@@ -334,8 +542,19 @@ inline void registered(){
 		if(use.password == "back"||use.password == "BACK"||use.password == "Back"){
     		wel_menu();
 		}
+		
+		    ifstream fi("user.txt");
+   			 string storedname, storedmail, storedPassword;
+			int storedid;
+	
+	while (fi >> storedid >>storedname >> storedmail >> storedPassword) {
+    	u_id = storedid;
+    } 
+    ++u_id;
+    fi.close();
+		
 	ofstream reg("user.txt",ios::app);
-	reg<<use.name<<"\t"<<use.mail<<"\t"<<use.password<<endl;
+	reg<<u_id<<"\t"<<use.name<<"\t"<<use.mail<<"\t"<<use.password<<endl;
 	reg.close();
 	cout<<"\n\t\t\t\tRegistered Successfully\n";
 	cout<<"\n\t\t\t\tLogin To Continue\n";
